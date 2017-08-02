9f8a96c1b728ef0c05828534e801517049664ca4
CDUTYangD <1442158384@qq.com>
2017-08-02 17:47:42 +0800

    恢复缓存

---
+++ OurMarket/src/ourmarket/ehcache/SayHelloTest.java
@@ -0,0 +1,20 @@
+package ourmarket.ehcache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration({"classpath:applicationContext.xml"}) 
+public class SayHelloTest {
+@Autowired ISayHello sayHello;
+	@Test
+	public void testSayhello() throws InterruptedException{
+		System.out.println("第一次调用：" + sayHello.getTimestamp("param"));
+        Thread.sleep(2000);
+        System.out.println("2秒后：" + sayHello.getTimestamp("param"));
+        Thread.sleep(11000);
+        System.out.println("11秒后：" + sayHello.getTimestamp("param"));
+	}
+}
